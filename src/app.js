bb8ebc91f6b3225f6d80f428d61d95567dffed0d
LuanSouzaSilveira <luansouzasilveira@gmail.com>
2020-09-01 10:42:35 -0300

    Config: Structuring GoogleApi

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const path = require("path");
+const { google } = require('googleapis');
+
+const googleCredentials = require('./google-oauth2.json').web;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+const OAuth2Client = new google.auth.OAuth2(
+  googleCredentials.client_id,
+  googleCredentials.client_secret,
+  googleCredentials.redirect_uris
+);
+
+google.options({auth: OAuth2Client})
+
+const url = OAuth2Client.generateAuthUrl({
+  access_type: 'offline',
+
+  scope: 'https://www.googleapis.com/auth/calendar'
+})
+
 app.use('/', (req, res) => {
-  res.render('login');
+  const url = OAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+  
+    scope: 'profile'
+  })
+
+  res.render('login', {url});
 })
 
 module.exports = app;
